194537fe2b28b16327818d97f9ae5efbb432c8f4
unknown <alex.jephtha@gmail.com>
2024-03-11 20:53:06 +1100

    added minimax templates

--- minimax.py
+++ minimax.py
@@ -1,15 +1,30 @@
 # Link: https://groklearning.com/learn/usyd-comp3308-2024-s1/adv-proj1/5/
 
+def UTILITY(state):
+  # TODO
+  pass
+
+def SCORE(state, player):
+  # TODO
+  pass
+
+def EVALUATION(state):
+  # TODO
+  pass
+
+def NUM_IN_A_ROW(state):
+  # TODO
+  pass 
+
 def decode(string):
   rows = string.split(",")
   grid = list(list(char for char in row) for row in rows) # rows on ouside lists - list of lists
   return grid
 
-
 def connect_four_mm(contents, turn, max_depth):
-    #TODO
-    return ''
+  # TODO
+  return ''
 
 if __name__ == '__main__':
-    # Example function call below, you can add your own to test the connect_four_mm function
-    connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1)
+  # Example function call below, you can add your own to test the connect_four_mm function
+  connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1)
