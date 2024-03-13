e8990e7ca7a26b4d95777c0cf37567644c9ee09e
jnic4234 <jnic4234@uni.sydney.edu.au>
2024-03-14 00:37:48 +1100

    msg

--- minimax.py
+++ minimax.py
@@ -34,6 +34,10 @@ def NUM_IN_A_ROW(count, state, player):
         piece = ""
         # throw error
         pass
+
+    len_2_count = 0
+    len_3_count = 0
+    len_4_count = 0
     
     # ROW TRAVERSALS - note if row has ≤1 token in it then there's no point checking the row/s above it
     for row in range(6):
@@ -41,10 +45,98 @@ def NUM_IN_A_ROW(count, state, player):
         adjacent_count = 0 
         for col in range(7):
             val = state[row][col]
-            # TODO: logic
             if val == piece:
+                adjacent_count += 1
+            else: # blank or other players piece
+                # record previous pieces in a row
+                if adjacent_count == 2:
+                    len_2_count += 2
+                elif adjacent_count == 3:
+                    len_3_count += 2
+                elif adjacent_count == 4:
+                    len_4_count += 2
+                # TODO: should be consider more cases (Seems like part 1 and 2 don't require it), there probably wouldn't be a point in predicting beyond 4 connected since that'd be the end of the game
+                # reset to 0 pieces in a row
+                adjacent_count = 0
+    
+    # COLUMN TRAVERSALS 
+    for col in range(7):
+        # for the current coloumn, iterate each row value of the column
+        adjacent_count = 0
+        for row in range(6):
+            val = state[row][col]
+            if val == piece:
+                adjacent_count += 1
+            else:
+                # record previous pieces in a row
+                if adjacent_count == 2:
+                    len_2_count += 2
+                elif adjacent_count == 3:
+                    len_3_count += 2
+                elif adjacent_count == 4:
+                    len_4_count += 2
+                # reset to 0 pieces in a row
+                adjacent_count = 0
+    
+    # NEGATIVE DIAGONAL TRAVERSALS (negative as in negative gradient) -> decrease row (down) and increase column (right)
+    # starting col = 0
+    for row in range(1,6):
+        col = 0
+        adjacent_count = 0
+        while row >= 0 and col <=6:
+            val = state[row][col]
+            if val == piece:
+                adjacent_count += 1
+            else:
+                # record previous pieces in a row
+                if adjacent_count == 2:
+                    len_2_count += 2
+                elif adjacent_count == 3:
+                    len_3_count += 2
+                elif adjacent_count == 4:
+                    len_4_count += 2
+                # reset to 0 pieces in a row
+                adjacent_count = 0
+            # update row and coloumn to continue down the diagonal
+            col += 1 # right
+            row -= 1 # down
+    # starting row = 5
+    for col in range(1,6):
+        row = 5
+        adjacent_count = 0
+        while row >= 0 and col <=6:
+            val = state[row][col]
+            if val == piece:
+                adjacent_count += 1
+            else:
+                # record previous pieces in a row
+                if adjacent_count == 2:
+                    len_2_count += 2
+                elif adjacent_count == 3:
+                    len_3_count += 2
+                elif adjacent_count == 4:
+                    len_4_count += 2
+                # reset to 0 pieces in a row
+                adjacent_count = 0
+            # update row and coloumn to continue down the diagonal
+            col += 1 # right
+            row -= 1 # down
+
+    # POSITIVE DIAGONAL TRAVERSALS
+    for col in range()
 
 
+
+
+
+
+
+
+
+
+
+
+            
 
 
 
